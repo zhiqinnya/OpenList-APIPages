d023901dad95ce78eea224adea2ae7eec59b388c
pikachuim <pikachuim@qq.com>
2025-07-17 14:56:03 +0800

    mod(driver): temp add aliyun drive return all message when fetch

--- src/driver/alicloud_oa.ts
+++ src/driver/alicloud_oa.ts
@@ -65,7 +65,7 @@ export async function alyLogin(c: Context) {
         false, "POST", "json",
         {'Content-Type': 'application/json'});
     // console.log(result);
-    if (!result.qrCodeUrl) return c.json({"text": result.message}, 500);
+    if (!result.qrCodeUrl) return c.json({"text": result.stringify()}, 500);
     return c.json({"text": result.qrCodeUrl, "sid": result.sid}, 200);
 }
 
